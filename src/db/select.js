c877bae665c8e50a8c89e0dbc9d7cdeb0cdb148d
Phillip G. Bradford <phillip.g.bradford@gmail.com>
2020-10-05 15:08:12 -0400

    lost select

---
+++ src/db/select.js
@@ -0,0 +1,47 @@
+const sql = require('mssql');
+
+var config =
+{
+	host: 'myserver99.database.windows.net',
+	user: 'db99admin',
+	password: '8suddsk!3',
+	server: 'myserver99.database.windows.net',
+	database: 'db99',
+	port: 1433
+};
+
+// ssl: true
+
+let data_query_table = {};
+
+/*
+ *
+ */
+data_query_table.selectData = (req, res) => {
+
+		// connect to your database
+    sql.connect(config, function (err) {
+
+	    if (err) console.log(err);
+
+	        // create Request object
+	    var request = new sql.Request();
+
+	        // query to the database and get the records
+	    request.query('select * from people', function (err, recordset) {
+
+			    if (err) console.log(err)
+
+					for (i = 0; i < recordset.length; i++) {
+						console.log('Row: ' + JSON.stringify(recordset[i]));
+					}
+					console.log('Done.');
+
+			            // send records as a response
+			    //res.send(recordset);
+
+    		});
+	});
+}
+
+module.exports = data_query_table;
